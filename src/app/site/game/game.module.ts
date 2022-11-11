198336d4211463e8e6ec5dd27e7732edc4ceb658
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-11-11 16:07:52 +0100

    mise en place page game - modification README.md

--- src/app/site/game/game.module.ts
+++ src/app/site/game/game.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameComponent } from './game.component';
-
+import { MaterialModule } from 'src/app/material-module';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
@@ -9,7 +10,9 @@ import { GameComponent } from './game.component';
     GameComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MaterialModule
   ]
 })
 export class GameModule { }
