12ee31074dc2c9074452bcdd13b05a3e0af5ca89
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-11-29 11:21:23 +0100

    ajout de : providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}] dans module.ts pour permettre le rechargement de la page en prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -43,7 +43,7 @@ import { ErrorModule } from './site/error/error.module';
     SharedModule,
     MaterialModule,
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
